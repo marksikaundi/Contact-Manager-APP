e150d2cc6778c04c10d93518ec0b44f110f6b219
marksikaundi <emmanuelmark117@gmail.com>
2022-02-26 01:03:39 +0200

    fetching data to frondend

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -4,6 +4,17 @@ import axios from "axios";
 export class ContactService {
     static serverURL = `http://localhost:9000`;
 
+    static getGroups(){
+        let dataURL = `${this.serverURL}/groups`;
+        return axios.get(dataURL);
+    }
+
+    static getGroup(contact){
+        let groupId = contact.groupId;
+        let dataURL = `${this.serverURL}/groups/${groupId}`;
+        return axios.get(dataURL);
+    }
+
     static getAllContacts(){
         let dataURL = `${this.serverURL}/contacts`;
         return axios.get(dataURL);
